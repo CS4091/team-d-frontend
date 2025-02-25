57d4e62597c76ac3ab3ed09a15541983b02c777d
ewbyf <ewbyf@umsystem.edu>
2025-02-24 22:31:18 -0600

    added map stuff and basic routing system

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,114 +1,126 @@
-import Image from "next/image";
-import { Geist, Geist_Mono } from "next/font/google";
+import { GoogleMap, InfoWindow, LoadScript, Marker } from '@react-google-maps/api';
+import { useEffect, useState } from 'react';
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
+const containerStyle = {
+	width: '100%',
+	height: '100%'
+};
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+const center = { lat: 39.8283, lng: -98.5795 };
 
-export default function Home() {
-  return (
-    <div
-      className={`${geistSans.variable} ${geistMono.variable} grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]`}
-    >
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/pages/index.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+interface Airport {
+	lat: number;
+	lng: number;
+	name: string;
 }
+
+function MapComponent() {
+	const [airports, setAirports] = useState<{ name: string; lat: number; lng: number }[]>([]);
+	const [selectedAirport, setSelectedAirport] = useState<Airport>({ lat: 0, lng: 0, name: 'None' });
+	const [selectedAirportList, setSelectedAirportList] = useState<Airport[]>([]);
+
+	useEffect(() => {
+		async function fetchAirports() {
+			const response = await fetch('/data/airports.csv');
+			const text = await response.text();
+			const rows = text.split('\n').slice(1);
+			const parsedAirports = rows
+				.map((row) => {
+					const cols = row.split(',');
+					return { name: cols[3], lat: parseFloat(cols[4]), lng: parseFloat(cols[5]) };
+				})
+				.filter((airport) => !isNaN(airport.lat) && !isNaN(airport.lng));
+
+			setAirports(parsedAirports);
+			console.log(parsedAirports);
+		}
+		fetchAirports();
+	}, []);
+
+	const addAirport = (airport: Airport) => {
+		if (selectedAirportList.find((air) => air == airport)) {
+			return;
+		}
+		setSelectedAirportList([...selectedAirportList, airport]);
+	};
+
+	const removeAirport = (airport: Airport) => {
+		if (!selectedAirportList.find((air) => air == airport)) {
+			return;
+		}
+        
+		setSelectedAirportList(selectedAirportList.filter((air) => air != airport));
+	};
+
+	return (
+		<div className='h-screen'>
+			<div className='absolute z-10 bg-white h-full w-1/4 py-4 px-4 gap-4 flex flex-col items-center shadow'>
+				<p className='text-xl text-center font-bold'>Airports Selected</p>
+				{selectedAirportList.map((airport) => (
+					<p className='text-lg text-center bg-gray-200 px-6 py-2 rounded-xl' key={airport.name}>
+						{airport.name}
+					</p>
+				))}
+				<button className='bg-blue-300 py-2 px-16 rounded-xl font-md hover:bg-blue-400'>Send</button>
+			</div>
+			<GoogleMap
+				mapContainerStyle={containerStyle}
+				center={center}
+				zoom={4}
+				options={{
+					minZoom: 3,
+					maxZoom: 50,
+					restriction: {
+						latLngBounds: {
+							north: 85,
+							south: -85,
+							west: -180,
+							east: 180
+						},
+						strictBounds: false
+					}
+				}}
+			>
+				{airports.map((airport, index) => (
+					<Marker
+						key={index}
+						position={{ lat: airport.lat, lng: airport.lng }}
+						title={airport.name}
+						onClick={() => {
+							setSelectedAirport(airport);
+						}}
+					/>
+				))}
+				{selectedAirport && selectedAirport.name != 'None' && (
+					<InfoWindow
+						position={{ lat: selectedAirport.lat, lng: selectedAirport.lng }}
+						onCloseClick={() => setSelectedAirport({ lat: 0, lng: 0, name: 'None' })}
+					>
+						<div className='flex flex-col gap-6 max-w-64'>
+							<p className='text-md font-bold text-center'>{selectedAirport.name}</p>
+							<div className='flex flex-col gap-2'>
+								{!selectedAirportList.find((air) => air == selectedAirport) && (
+									<button className='bg-blue-300 py-2 px-6 rounded-xl font-md hover:bg-blue-400' onClick={() => addAirport(selectedAirport)}>Add to Route</button>
+								)}
+								{selectedAirportList.find((air) => air == selectedAirport) && (
+									<button className='bg-red-300 py-2 px-6 rounded-xl font-md hover:bg-red-400' onClick={() => removeAirport(selectedAirport)}>Remove from Route</button>
+								)}
+							</div>
+						</div>
+					</InfoWindow>
+				)}
+			</GoogleMap>
+		</div>
+	);
+}
+
+const Home = () => {
+	return (
+		<LoadScript googleMapsApiKey={process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY!} libraries={['places']}>
+			<MapComponent />
+		</LoadScript>
+	);
+};
+
+export default Home;
+
