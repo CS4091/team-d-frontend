fb7b5aeb91bf1bcc2d9d3c4bd0fcda75ee0df49b
ewbyf <ewbyf@umsystem.edu>
2025-03-05 21:29:44 -0600

    added spline supp

---
+++ src/pages/test.tsx
@@ -0,0 +1,11 @@
+import Spline from '@splinetool/react-spline';
+
+export default function Test() {
+  return (
+    <main className=''>
+      <Spline
+        scene="https://prod.spline.design/P4Lyrknc4O6dVhf6/scene.splinecode" 
+      />
+    </main>
+  );
+}
