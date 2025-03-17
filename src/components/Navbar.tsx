f788d73f054436cafc552b28e4e0b78189baf9a4
ewbyf <ewbyf@umsystem.edu>
2025-03-16 20:11:42 -0500

    navbar update

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,22 +1,30 @@
 import Link from 'next/link';
-import { MdAccountCircle } from 'react-icons/md';
 
 const Navbar = () => {
 	return (
-		<div className="absolute z-10 bg-gradient-to-b from-black/50 via-black/17 to-black/0 h-12 w-full flex items-center justify-between px-12 py-8 text-white font-bold">
-			<div className=''></div>
-			<div className='w-full flex justify-center gap-8'>
-				<Link href="/" className="px-3 py-2 rounded-xl drop-shadow-[0px_0px_6px_rgba(0,0,0,0.25)] hover:bg-gray-700/20">
+		<div className='absolute z-10 bg-white h-16 w-full flex items-center justify-between px-12 py-8 text-primary font-bold'>
+			<div className='flex gap-16 items-center'>
+				<Link href='/' className='rounded-xl hover:text-blue-300'>
+					LOGO
+				</Link>
+				<Link href='/' className='rounded-xl hover:text-blue-300'>
 					Home
 				</Link>
-				<Link href="/routes" className="px-3 py-2 rounded-xl drop-shadow-[0px_0px_6px_rgba(0,0,0,0.25)] hover:bg-gray-700/20">
-					Routes
+				<Link href='/dashboard' className='rounded-xl hover:text-blue-300'>
+					Dashboard
 				</Link>
-				<Link href="/" className="px-3 py-2 rounded-xl drop-shadow-[0px_0px_6px_rgba(0,0,0,0.25)] hover:bg-gray-700/20">
+				<Link href='/' className='rounded-xl hover:text-blue-300'>
 					History
 				</Link>
 			</div>
-			<MdAccountCircle size={32} className="ml-auto cursor-pointer" />
+			<div className='flex gap-4 items-center'>
+				<Link href='/' className='px-5 py-2 rounded-xl hover:bg-[#E1F1FF]'>
+					Log in
+				</Link>
+				<Link href='/' className='px-5 py-2 bg-primary text-white rounded-xl hover:bg-[#8CB4FF]'>
+					Sign up
+				</Link>
+			</div>
 		</div>
 	);
 };
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,6 +11,10 @@ export default {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        primary: '#669BFF',
+        secondary: '#4F4F4F',
+        accent: '#FF61E2',
+        text: '#020202',
       },
     },
   },
