eaa8c74c05bbc3772c98a20ee3339db378cf4338
ewbyf <ewbyf@umsystem.edu>
2025-03-17 18:12:59 -0500

    added more font options

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,22 +1,23 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 export default {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-        primary: '#669BFF',
-        secondary: '#4F4F4F',
-        accent: '#FF61E2',
-        text: '#020202',
-      },
-    },
-  },
-  plugins: [],
+	content: ['./src/pages/**/*.{js,ts,jsx,tsx,mdx}', './src/components/**/*.{js,ts,jsx,tsx,mdx}', './src/app/**/*.{js,ts,jsx,tsx,mdx}'],
+	theme: {
+		extend: {
+			colors: {
+				background: 'var(--background)',
+				foreground: 'var(--foreground)',
+				primary: '#669BFF',
+				secondary: '#4F4F4F',
+				accent: '#FF61E2',
+				text: '#020202'
+			},
+			fontFamily: {
+				merriweather: ['Merriweather', 'sans-serif'],
+				raleway: ['Raleway', 'sans-serif']
+			}
+		}
+	},
+	plugins: []
 } satisfies Config;
+
