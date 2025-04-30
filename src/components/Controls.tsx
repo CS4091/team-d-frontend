0f1b43bb0c5074e79d24e6ae59297b1a1a255bf1
Eric Wong <107148516+ewbyf@users.noreply.github.com>
2025-04-30 17:30:23 -0500

    view chart

--- src/components/Controls.tsx
+++ src/components/Controls.tsx
@@ -1,6 +1,8 @@
 import { Asset } from '@/interfaces/Asset';
 import { Route } from '@/interfaces/Route';
 import GenerateRouteButton from './GenerateRouteButton';
+import { useState } from 'react';
+import { Eye } from 'lucide-react';
 
 interface Props {
 	selectedOrganization: string;
@@ -11,18 +13,31 @@ interface Props {
 }
 
 const Controls = ({ selectedOrganization, routeList, inventory, addPolylines }: Props) => {
+	const [open, setOpen] = useState(false);
+	const [hasRoute, setHasRoute] = useState(false);
+
 	return (
 		<div className="fixed bottom-4 right-4 border-[1px] border-white/25 bg-gradient-to-br from-white/20 to-white/5 backdrop-blur md:rounded-2xl z-[48] p-6 flex flex-col gap-4">
-			{/* <button
-				className='bg-pink py-3 px-12 rounded-xl text-white font-bold hover:bg-pinkhover'
-			
+			<button
+				disabled={!hasRoute}
+				onClick={() => setOpen(true)}
+				className="relative bg-gradient-to-r from-primary to-indigo-300 bg-[length:200%_200%] bg-[position:0%_50%] py-3 px-12 rounded-xl text-white font-bold transition-all duration-500 ease-in-out enabled:hover:bg-[position:100%_50%] enabled:hover:shadow-xl disabled:opacity-50"
 			>
-				View Past Routes
-			</button> */}
-			<GenerateRouteButton selectedOrganization={selectedOrganization} routeList={routeList} inventory={inventory} addPolylines={addPolylines} />
+				<Eye className="inline mr-2 relative z-10" size={20} />
+				<span className="relative z-10">View Last Chart</span>
+			</button>
+
+			<GenerateRouteButton
+				setHasRoute={setHasRoute}
+				open={open}
+				setOpen={setOpen}
+				selectedOrganization={selectedOrganization}
+				routeList={routeList}
+				inventory={inventory}
+				addPolylines={addPolylines}
+			/>
 		</div>
 	);
 };
 
 export default Controls;
-
--- src/components/GenerateRouteButton.tsx
+++ src/components/GenerateRouteButton.tsx
@@ -13,7 +13,9 @@ interface Props {
 	selectedOrganization: string;
 	routeList: Route[];
 	inventory: Asset[];
-
+	open: boolean;
+	setOpen: React.Dispatch<React.SetStateAction<boolean>>;
+	setHasRoute: React.Dispatch<React.SetStateAction<boolean>>;
 	addPolylines: (flights: string[][]) => void;
 }
 
@@ -44,9 +46,8 @@ interface GeneratedRoute {
 	};
 }
 
-const GenerateRouteButton = ({ selectedOrganization, routeList, inventory, addPolylines }: Props) => {
+const GenerateRouteButton = ({ setHasRoute, open, setOpen, selectedOrganization, routeList, inventory, addPolylines }: Props) => {
 	const [loading, setLoading] = useState(false);
-	const [open, setOpen] = useState(false);
 	const [routeData, setRouteData] = useState<GeneratedRoute | null>(null);
 
 	const calculateRoute = () => {
@@ -54,13 +55,14 @@ const GenerateRouteButton = ({ selectedOrganization, routeList, inventory, addPo
 		const transformedArray = routeList.map((item) => ({
 			from: item.from.id,
 			to: item.to.id,
-			passengers: item.passengers
+			passengers: item.passengers,
 		}));
 		api.post<GeneratedRoute>('/aviation/route', {
 			organizationId: selectedOrganization,
-			demand: transformedArray
+			demand: transformedArray,
 		})
 			.then((resp) => {
+				setHasRoute(true);
 				const data = resp.data;
 
 				// Calculate total time for baseline and optimized routes
@@ -79,16 +81,16 @@ const GenerateRouteButton = ({ selectedOrganization, routeList, inventory, addPo
 						...data.baseline,
 						stats: {
 							...data.baseline.stats,
-							time: baselineTotalTime
-						}
+							time: baselineTotalTime,
+						},
 					},
 					optimized: {
 						...data.optimized,
 						stats: {
 							...data.optimized.stats,
-							time: optimizedTotalTime
-						}
-					}
+							time: optimizedTotalTime,
+						},
+					},
 				});
 
 				setLoading(false);
@@ -115,47 +117,47 @@ const GenerateRouteButton = ({ selectedOrganization, routeList, inventory, addPo
 			<button
 				onClick={calculateRoute}
 				disabled={routeList.length === 0 || loading}
-				className='relative bg-gradient-to-r from-pink to-primary bg-[length:200%_200%] bg-[position:0%_50%] py-3 px-12 rounded-xl text-white font-bold transition-all duration-500 ease-in-out enabled:hover:bg-[position:100%_50%] enabled:hover:shadow-xl disabled:opacity-50'
+				className="relative bg-gradient-to-r from-pink to-purple-400 bg-[length:200%_200%] bg-[position:0%_50%] py-3 px-12 rounded-xl text-white font-bold transition-all duration-500 ease-in-out enabled:hover:bg-[position:100%_50%] enabled:hover:shadow-xl disabled:opacity-50"
 			>
 				{!loading && (
 					<>
-						<WandSparkles className='inline mr-2 relative z-10' size={20} />
-						<span className='relative z-10'>Generate Route</span>
+						<WandSparkles className="inline mr-2 relative z-10" size={20} />
+						<span className="relative z-10">Generate Route</span>
 					</>
 				)}
 				{loading && (
-					<div className='flex items-center justify-center gap-2'>
-						<Oval visible={true} height='16' width='16' wrapperStyle={{}} wrapperClass='' strokeWidth={6} color={'white'} />
+					<div className="flex items-center justify-center gap-2">
+						<Oval visible={true} height="16" width="16" wrapperStyle={{}} wrapperClass="" strokeWidth={6} color={'white'} />
 						<p>Generating</p>
 					</div>
 				)}
 			</button>
 
-			<DialogContent className='bg-[#ffffff] max-w-3xl max-h-[90vh] overflow-y-auto'>
+			<DialogContent className="bg-[#ffffff] max-w-3xl max-h-[90vh] overflow-y-auto">
 				<DialogHeader>
-					<DialogTitle className='text-2xl font-bold'>Route Comparison Chart</DialogTitle>
+					<DialogTitle className="text-2xl font-bold">Route Comparison Chart</DialogTitle>
 				</DialogHeader>
 
-				<div className='flex flex-col gap-2 p-4'>
-					<div className='grid grid-cols-2 gap-x-8'>
-						<div className='flex flex-col'>
-							<p className='font-bold text-xl mb-1'>Baseline Route</p>
-							<div className='flex items-center mb-2 mt-1'>
-								<p className='mb-2 mt-1 mr-2 font-semibold text-sm font-merriweather bg-gray-500 px-4 py-1 w-fit rounded-full text-white'>
+				<div className="flex flex-col gap-2 p-4">
+					<div className="grid grid-cols-2 gap-x-8">
+						<div className="flex flex-col">
+							<p className="font-bold text-xl mb-1">Baseline Route</p>
+							<div className="flex items-center mb-2 mt-1">
+								<p className="mb-2 mt-1 mr-2 font-semibold text-sm font-merriweather bg-gray-500 px-4 py-1 w-fit rounded-full text-white">
 									{routeData?.baseline.stats.time.toFixed(2)} hrs
 								</p>
-								<p className='mb-2 mt-1 mr-2 font-semibold text-sm font-merriweather bg-gray-700 px-4 py-1 w-fit rounded-full text-white'>
+								<p className="mb-2 mt-1 mr-2 font-semibold text-sm font-merriweather bg-gray-700 px-4 py-1 w-fit rounded-full text-white">
 									${routeData?.baseline.stats.fuel.toFixed(2).replace(/\B(?=(\d{3})+(?!\d))/g, ',')}
 								</p>
 							</div>
 						</div>
-						<div className='flex flex-col'>
-							<p className='font-bold text-xl mb-1'>Optimized Route</p>
-							<div className='flex items-center mb-2 mt-1'>
-								<p className='mr-2 font-semibold text-sm font-merriweather bg-primary px-4 py-1 w-fit rounded-full text-white'>
+						<div className="flex flex-col">
+							<p className="font-bold text-xl mb-1">Optimized Route</p>
+							<div className="flex items-center mb-2 mt-1">
+								<p className="mr-2 font-semibold text-sm font-merriweather bg-primary px-4 py-1 w-fit rounded-full text-white">
 									{routeData?.optimized.stats.time.toFixed(2)} hrs
 								</p>
-								<p className='mr-2 font-semibold text-sm font-merriweather bg-pink px-4 py-1 w-fit rounded-full text-white'>
+								<p className="mr-2 font-semibold text-sm font-merriweather bg-pink px-4 py-1 w-fit rounded-full text-white">
 									${routeData?.optimized.stats.fuel.toFixed(2).replace(/\B(?=(\d{3})+(?!\d))/g, ',')}
 								</p>
 
@@ -164,7 +166,7 @@ const GenerateRouteButton = ({ selectedOrganization, routeList, inventory, addPo
 									color={routeData?.baseline.stats.fuel! - routeData?.optimized.stats.fuel! > 0 ? '#3BC183' : '#757575'}
 								/>
 								<p
-									className='text-[#3BC183] font-semibold text-sm font-merriweather -ml-1'
+									className="text-[#3BC183] font-semibold text-sm font-merriweather -ml-1"
 									style={{ color: routeData?.baseline.stats.fuel! - routeData?.optimized.stats.fuel! > 0 ? '#3BC183' : '#757575' }}
 								>
 									${(routeData?.baseline.stats.fuel! - routeData?.optimized.stats.fuel!).toFixed(2).replace(/\B(?=(\d{3})+(?!\d))/g, ',')}
@@ -173,19 +175,19 @@ const GenerateRouteButton = ({ selectedOrganization, routeList, inventory, addPo
 						</div>
 					</div>
 
-					<div className='grid grid-cols-2 gap-x-8 gap-y-3'>
+					<div className="grid grid-cols-2 gap-x-8 gap-y-3">
 						{inventory.map((inv) => (
 							<>
 								{routeData?.baseline.routing[inv.id] && routeData?.baseline.routing[inv.id].length > 1 && (
-									<div className='flex flex-col'>
-										<div className='flex gap-1 whitespace-nowrap'>
-											<p className='font-bold'>{inv.manufacturer} -</p>
-											<p className='font-bold font-merriweather'>{inv.model}</p>
+									<div className="flex flex-col">
+										<div className="flex gap-1 whitespace-nowrap">
+											<p className="font-bold">{inv.manufacturer} -</p>
+											<p className="font-bold font-merriweather">{inv.model}</p>
 										</div>
-										<div className='flex flex-wrap items-center max-w-full overflow-y-auto overflow-x-hidden break-words'>
+										<div className="flex flex-wrap items-center max-w-full overflow-y-auto overflow-x-hidden break-words">
 											{(routeData?.baseline.routing[inv.id] || []).map((port, i, arr) => (
-												<span key={i} className='flex items-center'>
-													<p className='break-words whitespace-normal'>{port}</p>
+												<span key={i} className="flex items-center">
+													<p className="break-words whitespace-normal">{port}</p>
 													{i < arr.length - 1 && <MdArrowRightAlt size={20} />}
 												</span>
 											))}
@@ -193,17 +195,17 @@ const GenerateRouteButton = ({ selectedOrganization, routeList, inventory, addPo
 									</div>
 								)}
 								{routeData?.optimized.routing[inv.id] && routeData?.optimized.routing[inv.id].length > 1 && (
-									<div className='flex flex-col'>
-										<div className='flex gap-1 whitespace-nowrap'>
-											<p className='font-bold'>{inv.manufacturer} -</p>
-											<p className='font-bold font-merriweather'>{inv.model}</p>
+									<div className="flex flex-col">
+										<div className="flex gap-1 whitespace-nowrap">
+											<p className="font-bold">{inv.manufacturer} -</p>
+											<p className="font-bold font-merriweather">{inv.model}</p>
 										</div>
-										<div className='flex flex-wrap items-center max-w-full overflow-auto'>
+										<div className="flex flex-wrap items-center max-w-full overflow-auto">
 											{routeData?.optimized.routing[inv.id] &&
 												routeData?.optimized.routing[inv.id].length > 1 &&
 												(routeData?.optimized.routing[inv.id] || []).map((port, i, arr) => (
-													<span key={i} className='flex items-center'>
-														<p className='whitespace-nowrap'>{port}</p>
+													<span key={i} className="flex items-center">
+														<p className="whitespace-nowrap">{port}</p>
 														{i < arr.length - 1 && <MdArrowRightAlt size={20} />}
 													</span>
 												))}
