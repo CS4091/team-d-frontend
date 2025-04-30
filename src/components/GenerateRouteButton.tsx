f8f41e82945a9d200c730ce357526f66e18545f9
ewbyf <ewbyf@umsystem.edu>
2025-04-30 14:07:03 -0500

    added scroll

--- src/components/GenerateRouteButton.tsx
+++ src/components/GenerateRouteButton.tsx
@@ -74,42 +74,44 @@ const GenerateRouteButton = ({ selectedOrganization, routeList, inventory, addPo
 			open={open}
 			onOpenChange={(isOpen) => {
 				setOpen(isOpen);
-			}}>
+			}}
+		>
 			<button
 				onClick={calculateRoute}
 				disabled={routeList.length === 0 || loading}
-				className="relative bg-gradient-to-r from-pink to-primary bg-[length:200%_200%] bg-[position:0%_50%] py-3 px-12 rounded-xl text-white font-bold transition-all duration-500 ease-in-out enabled:hover:bg-[position:100%_50%] enabled:hover:shadow-xl disabled:opacity-50">
+				className='relative bg-gradient-to-r from-pink to-primary bg-[length:200%_200%] bg-[position:0%_50%] py-3 px-12 rounded-xl text-white font-bold transition-all duration-500 ease-in-out enabled:hover:bg-[position:100%_50%] enabled:hover:shadow-xl disabled:opacity-50'
+			>
 				{!loading && (
 					<>
-						<WandSparkles className="inline mr-2 relative z-10" size={20} />
-						<span className="relative z-10">Generate Route</span>
+						<WandSparkles className='inline mr-2 relative z-10' size={20} />
+						<span className='relative z-10'>Generate Route</span>
 					</>
 				)}
 				{loading && (
-					<div className="flex items-center justify-center gap-2">
-						<Oval visible={true} height="16" width="16" wrapperStyle={{}} wrapperClass="" strokeWidth={6} color={'white'} />
+					<div className='flex items-center justify-center gap-2'>
+						<Oval visible={true} height='16' width='16' wrapperStyle={{}} wrapperClass='' strokeWidth={6} color={'white'} />
 						<p>Generating</p>
 					</div>
 				)}
 			</button>
 
-			<DialogContent className="bg-[#ffffff] max-w-3xl">
+			<DialogContent className='bg-[#ffffff] max-w-3xl max-h-[90vh] overflow-y-auto'>
 				<DialogHeader>
-					<DialogTitle className="text-2xl font-bold">Route Comparison Chart</DialogTitle>
+					<DialogTitle className='text-2xl font-bold'>Route Comparison Chart</DialogTitle>
 				</DialogHeader>
 
-				<div className="flex flex-col gap-2 p-4">
-					<div className="grid grid-cols-2 gap-x-8">
-						<div className="flex flex-col">
-							<p className="font-bold text-xl mb-1">Baseline Route</p>
-							<p className="mb-2 mt-1 mr-2 font-semibold text-sm font-merriweather bg-gray-600 px-4 py-1 w-fit rounded-full text-white">
+				<div className='flex flex-col gap-2 p-4'>
+					<div className='grid grid-cols-2 gap-x-8'>
+						<div className='flex flex-col'>
+							<p className='font-bold text-xl mb-1'>Baseline Route</p>
+							<p className='mb-2 mt-1 mr-2 font-semibold text-sm font-merriweather bg-gray-600 px-4 py-1 w-fit rounded-full text-white'>
 								${routeData?.baseline.stats.fuel.toFixed(2).replace(/\B(?=(\d{3})+(?!\d))/g, ',')}
 							</p>
 						</div>
-						<div className="flex flex-col">
-							<p className="font-bold text-xl mb-1">Optimized Route</p>
-							<div className="flex items-center mb-2 mt-1">
-								<p className="mr-2 font-semibold text-sm font-merriweather bg-pink px-4 py-1 w-fit rounded-full text-white">
+						<div className='flex flex-col'>
+							<p className='font-bold text-xl mb-1'>Optimized Route</p>
+							<div className='flex items-center mb-2 mt-1'>
+								<p className='mr-2 font-semibold text-sm font-merriweather bg-pink px-4 py-1 w-fit rounded-full text-white'>
 									${routeData?.optimized.stats.fuel.toFixed(2).replace(/\B(?=(\d{3})+(?!\d))/g, ',')}
 								</p>
 
@@ -118,40 +120,44 @@ const GenerateRouteButton = ({ selectedOrganization, routeList, inventory, addPo
 									color={routeData?.baseline.stats.fuel! - routeData?.optimized.stats.fuel! > 0 ? '#3BC183' : '#757575'}
 								/>
 								<p
-									className="text-[#3BC183] font-semibold text-sm font-merriweather -ml-1"
-									style={{ color: routeData?.baseline.stats.fuel! - routeData?.optimized.stats.fuel! > 0 ? '#3BC183' : '757575' }}>
+									className='text-[#3BC183] font-semibold text-sm font-merriweather -ml-1'
+									style={{ color: routeData?.baseline.stats.fuel! - routeData?.optimized.stats.fuel! > 0 ? '#3BC183' : '757575' }}
+								>
 									${(routeData?.baseline.stats.fuel! - routeData?.optimized.stats.fuel!).toFixed(2).replace(/\B(?=(\d{3})+(?!\d))/g, ',')}
 								</p>
 							</div>
 						</div>
 					</div>
 
-					<div className="grid grid-cols-2 gap-x-8 gap-y-3">
+					<div className='grid grid-cols-2 gap-x-8 gap-y-3'>
 						{inventory.map((inv) => (
 							<>
-								<div className="flex flex-col">
-									<div className="flex gap-1 whitespace-nowrap">
-										<p className="font-bold">{inv.manufacturer} -</p>
-										<p className="font-bold font-merriweather">{inv.model}</p>
+								<div className='flex flex-col'>
+									<div className='flex gap-1 whitespace-nowrap'>
+										<p className='font-bold'>{inv.manufacturer} -</p>
+										<p className='font-bold font-merriweather'>{inv.model}</p>
 									</div>
-									<div className="flex flex-wrap items-center max-w-full overflow-auto">
+                                    <div className='flex flex-wrap items-center max-w-full overflow-y-auto overflow-x-hidden break-words'>
+
 										{(routeData?.baseline.routing[inv.id] || []).map((port, i, arr) => (
-											<span key={i} className="flex items-center">
-												<p className="whitespace-nowrap">{port}</p>
+											<span key={i} className='flex items-center'>
+												<p className='break-words whitespace-normal'>
+													{port}
+												</p>
 												{i < arr.length - 1 && <MdArrowRightAlt size={20} />}
 											</span>
 										))}
 									</div>
 								</div>
-								<div className="flex flex-col">
-									<div className="flex gap-1 whitespace-nowrap">
-										<p className="font-bold">{inv.manufacturer} -</p>
-										<p className="font-bold font-merriweather">{inv.model}</p>
+								<div className='flex flex-col'>
+									<div className='flex gap-1 whitespace-nowrap'>
+										<p className='font-bold'>{inv.manufacturer} -</p>
+										<p className='font-bold font-merriweather'>{inv.model}</p>
 									</div>
-									<div className="flex flex-wrap items-center max-w-full overflow-auto">
+									<div className='flex flex-wrap items-center max-w-full overflow-auto'>
 										{(routeData?.optimized.routing[inv.id] || []).map((port, i, arr) => (
-											<span key={i} className="flex items-center">
-												<p className="whitespace-nowrap">{port}</p>
+											<span key={i} className='flex items-center'>
+												<p className='whitespace-nowrap'>{port}</p>
 												{i < arr.length - 1 && <MdArrowRightAlt size={20} />}
 											</span>
 										))}
@@ -167,4 +173,3 @@ const GenerateRouteButton = ({ selectedOrganization, routeList, inventory, addPo
 };
 
 export default GenerateRouteButton;
-
