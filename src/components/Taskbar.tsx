c361df3a645b43167dfd92a891ecf36d9b5f22c1
Eric Wong <107148516+ewbyf@users.noreply.github.com>
2025-04-30 17:35:28 -0500

    taskbar update

--- src/components/Taskbar.tsx
+++ src/components/Taskbar.tsx
@@ -1,4 +1,6 @@
+import { UserContext } from '@/lib/context';
 import { Building, Plane, Route } from 'lucide-react';
+import { useContext } from 'react';
 
 interface Props {
 	openRoutesPanel: boolean;
@@ -17,26 +19,30 @@ const Taskbar = ({
 	setOpenInventoryPanel,
 	setOpenOrganizationPanel,
 }: Props) => {
+	const { selectedOrganization } = useContext(UserContext);
+
 	return (
 		<div className="fixed bottom-4 left-1/2 transform -translate-x-1/2  border-[1px] border-white/25 bg-gradient-to-br from-white/20 to-white/5 backdrop-blur md:rounded-2xl z-[48] p-4 flex gap-4">
-			<div
+			<button
 				className="rounded-full p-2 inset-0 z-0 hover:bg-gradient-to-br from-white/20 to-white/5 transition-all group-hover:opacity-100 hover:scale-105 active:scale-95 cursor-pointer"
 				onClick={() => setOpenOrganizationPanel(!openOrganizationPanel)}
 			>
 				<Building size={24} color={openOrganizationPanel ? 'white' : 'gray'} />
-			</div>
-			<div
-				className="rounded-full p-2  inset-0 z-0 hover:bg-gradient-to-br from-white/20 to-white/5 transition-all group-hover:opacity-100 hover:scale-105 active:scale-95 cursor-pointer"
+			</button>
+			<button
+				className="rounded-full p-2  inset-0 z-0 enabled:hover:bg-gradient-to-br from-white/20 to-white/5 transition-all enabled:group-hover:opacity-100 enabled:hover:scale-105 enabled:active:scale-95 enabled:cursor-pointer"
 				onClick={() => setOpenRoutesPanel(!openRoutesPanel)}
+				disabled={selectedOrganization == ''}
 			>
-				<Route size={24} color={openRoutesPanel ? 'white' : 'gray'} />
-			</div>
-			<div
-				className="rounded-full p-2  inset-0 z-0 hover:bg-gradient-to-br from-white/20 to-white/5 transition-all group-hover:opacity-100 hover:scale-105 active:scale-95 cursor-pointer"
+				<Route size={24} color={openRoutesPanel && selectedOrganization != "" ? 'white' : 'gray'} />
+			</button>
+			<button
+				className="rounded-full p-2 inset-0 z-0 enabled:hover:bg-gradient-to-br from-white/20 to-white/5 transition-all enabled:group-hover:opacity-100 enabled:hover:scale-105 enabled:active:scale-95 enabled:cursor-pointer"
 				onClick={() => setOpenInventoryPanel(!openInventoryPanel)}
+				disabled={selectedOrganization == ''}
 			>
-				<Plane size={24} color={openInventoryPanel ? 'white' : 'gray'} />
-			</div>
+				<Plane size={24} color={openInventoryPanel && selectedOrganization != ""  ? 'white' : 'gray'} />
+			</button>
 		</div>
 	);
 };
